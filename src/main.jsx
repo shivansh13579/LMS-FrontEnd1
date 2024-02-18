69a37a5ec47e652fd778df52740082ad1d2b78eb
shivansh13579 <shivansh0975@gmail.com>
2024-02-18 22:16:02 +0530

    create redux store and also create footer section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import store from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+   <Provider store={store}>
+   <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <Toaster />
+   </BrowserRouter>
+   </Provider>
 )
