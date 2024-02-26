e3327505f686fc8eab7d9368d9c4ad6bac26f8ad
shivansh13579 <shivansh0975@gmail.com>
2024-02-26 22:21:23 +0530

    logout funcationality is created

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -29,11 +29,10 @@ function HomeLayout({ children }) {
         drawerSide[0].style.width = '0';
     }
 
-    function handleLogout(e){
+   async function handleLogout(e){
            e.preventDefault();
-
-           //const res = await dispatch(logout());
-           //if(res?.payload?.success)
+           const res = await dispatch(logout());
+           if(res?.payload?.success)
            navigate('/');
     }
 
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -42,6 +42,22 @@ export const login = createAsyncThunk("/auth/login", async (data) => {
     }
 })
 
+export const logout = createAsyncThunk("/auth/logout", async () => {
+    try {
+        const res = axiosInstance.post("user/logout");
+        toast.promise(res, {
+            loading: "Wait! logout in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to logout"
+        });
+        return (await res).data;
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+})
+
 const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -49,6 +65,17 @@ const authSlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(login.fulfilled, (state,action) =>{
             localStorage.setItem("data",JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role",action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.role;
+        })
+        .addCase(logout.fulfilled, (state,action) => {
+            localStorage.clear();
+            state.data = {};
+            state.isLoggedIn = false;
+            state.role = "";
         })
     }
 })
